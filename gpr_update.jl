b2f3cc3294653b734f361d6697f1b61f0bbe63cc
Roel Stappers <roel.stappers@met.no>
2020-01-17 12:55:24 +0100

    Update grp

--- gpr_update.jl
+++ gpr_update.jl
@@ -1,80 +1,55 @@
 using DataFrames
-import TimeSeries
-import CSV
-using Glob
+# import CSV
+using Statistics
 using Dates
 using Plots
-import Netatmo
-import PyPlot
+using Netatmo
+# import PyPlot
 using LinearAlgebra
 using IterativeSolvers
 
 dtg = Dates.DateTime(2018,05,10,16)
-period = Dates.Hour(2)
+period = Dates.Day(10)
+timerange = dtg:Hour(1):dtg+period
 
-timerange = datetime2unix(dtg):60*10:datetime2unix(dtg+period)
 latrange  = 59.9:0.01:60  
 lonrange  = 10.7:0.01:10.8
 latrange  = 59:0.01:60
 lonrange  = 7.:0.01:8
 
 df = Netatmo.read(timerange, latrange=latrange, lonrange=lonrange);
-
 println("Finished reading $(size(df,1)) rows")
 
+# Add column to store GP estimated obs
+df[!,:pressurehat] .= 0.0
 
-function RBF(row1,row2) 
-    lt = 1.0/(2.0*(1.0*60.0*60.0)^2) # time length scale 
-    lx = 1.0/(2.0*(0.01)^2)       # latlon length scale 
-    dtsq   = -lt*(row1[:time_utc] - row2[:time_utc])^2
-    dlatsq = -lx*(row1[:lat] - row2[:lat])^2
-    dlonsq = -lx*(row1[:lon] - row2[:lon])^2    
-    return exp(dlatsq + dlonsq + dtsq)
-end
+groupedbyid = groupby(df,:id)
 
+sigmat  = 3*60*60   # time correlation length scale 6 hours
+sigmao  = 0.2    # hPa
 
+rbf(utc1,utc2) = exp(-(utc1-utc2)^2/(2*sigmat^2))
 
-sigmao = 1  
-
-K   = [RBF(r1,r2) for r1 in eachrow(df), r2 in eachrow(df)] 
-
-groupbyid = DataFrames.groupby(df,:id)
-by(df,:id) do s1
-    s1[:pressure] - Statistics.mean(s1[:pressure])
-    DataFrames.DataFrame(anom=s1[:pressure])
-end
-
-lt = 1.0/(2.0*(6.0*60.0*60.0)^2) # time length scale 
-lx = 1.0/(2.0*(0.1)^2)       # latlon length scale     
-for s1 in groupbyid
-   for s2 in groupbyid 
-      dlatsq = -lx*(s1[:lat][1] - s2[:lat][1])^2
-      dlonsq = -lx*(s1[:lon][1] - s2[:lon][1])^2 
-      c1 = exp(dlatsq+dlonsq)   
-      println(s1[:time_utc]) 
-      for t1 in s1[:time_utc]    
-         for t2 in s2[:time_utc]
-            
-            dtsq = -lt*(t1 - t2)^2          
-            K[s1[:id][1],s2[:id],t1,t2] = c1*exp(dtsq)
-         end                  
-      end 
-   end 
-end
-
-KpI = K  
-KpI[diagind(KpI)] .= 1 + sigmao^2
-
-q,minreslog= minres(KpI,df[:pressure],log=true)
+# estimator(pressure) = 
 
 
-# K_s  = [RBF(r1,r2) for t1 in tf, t2 in timerange]
-# K_ss = [RBFl(t1,t2) for t1 in timerange, t2 in timerange]
-#   K[diagind(K)] .= 1 + sigmao^2
-#q,log= minres(K,s1u[:pressure],log=true)
-#   smooth = K_s'* q
+for station in groupedbyid[1:2]
+   K = [rbf(r1[:time_utc],r2[:time_utc]) for r1 in eachrow(station), r2 in eachrow(station)] 
+   KpI = copy(K) 
+   KpI[diagind(KpI)] .= 1 + sigmao^2
+   meanpres = mean(station[!,:pressure])
+   pressureanom = station[!,:pressure] .- meanpres
+   q, minreslog = minres(KpI,pressureanom,log=true)
    
-#   plot!(p,unix2datetime.(tf), s1u[:pressure],m=:o,markersize=1)
-#   plot!(p,unix2datetime.(timerange),smooth)
-#end
-#p
+   pressurehat = K*q .+ meanpres
+   for i in 1:nrow(station)
+      station[i,:pressurehat] = pressurehat[i]
+   end 
+   absdiff = abs.(station[!,:pressurehat]-station[!,:pressure])
+   println("$(maximum(absdiff)) $(station[1,:id]) $minreslog")
+   if any(absdiff .> 3*sigmao)
+      println("stationerror")
+   end 
+end 
+scatter(unix2datetime.(groupedbyid[1][!,:time_utc]),groupedbyid[1][!,:pressure],markersize=0.3)
+plot!(unix2datetime.(groupedbyid[1][!,:time_utc]),groupedbyid[1][!,:pressurehat])
